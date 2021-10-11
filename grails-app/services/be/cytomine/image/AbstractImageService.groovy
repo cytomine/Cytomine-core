d37115a0f2ee227dfeba889dfd9cddb8d85ff3c1
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2021-10-11 16:22:30 +0200

    replace checkAtLeastOne for abstractimage permissions checking

--- grails-app/services/be/cytomine/image/AbstractImageService.groovy
+++ grails-app/services/be/cytomine/image/AbstractImageService.groovy
@@ -326,8 +326,7 @@ class AbstractImageService extends ModelService {
      * @return  Response structure (new domain data, old domain data..)
      */
     def update(AbstractImage image,def jsonNewData) throws CytomineException {
-
-        securityACLService.checkAtLeastOne(image,WRITE)
+        securityACLService.check(image.container())
         SecUser currentUser = cytomineService.getCurrentUser()
         def attributes = JSON.parse(image.encodeAsJSON())
         def res = executeCommand(new EditCommand(user: currentUser), image,jsonNewData)
