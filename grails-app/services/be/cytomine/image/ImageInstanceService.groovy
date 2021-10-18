bc4acbe4a4db9b311cff1f5ef2110e03849c8773
TheGeekTortoise <Renaud.Hoyoux@yahoo.fr>
2021-10-18 16:24:56 +0200

    improve not copied abstract image properties to image instance

--- grails-app/services/be/cytomine/image/ImageInstanceService.groovy
+++ grails-app/services/be/cytomine/image/ImageInstanceService.groovy
@@ -640,7 +640,9 @@ class ImageInstanceService extends ModelService {
             if(! (it.key.startsWith("cytomine.") || it.key.startsWith("mimeType") || it.key.startsWith("openslide.") ||
                     it.key.startsWith("hamamatsu.") || it.key.startsWith("tiff.") || it.key.startsWith("JFIF.") ||
                     it.key.startsWith("File.")  || it.key.startsWith("EXIF.") || it.key.startsWith("mirax.") ||
-                    it.key.startsWith("XMP.") || it.key.startsWith("ventana.") || it.key.startsWith("ExifTool.") )){
+                    it.key.startsWith("XMP.") || it.key.startsWith("ventana.") || it.key.startsWith("ExifTool.") ||
+                    it.key.startsWith("MakerNotes.") || it.key.startsWith("PNG.") || it.key.startsWith("MPF.")
+            )){
                 Property p= new Property(key: it.key, value: it.value)
                 p.setDomain(domain)
                 propertyService.add(JSON.parse(p.encodeAsJSON()))
