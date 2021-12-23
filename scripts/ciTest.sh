75bd08660ccf9ef1f2c25401a44c8bb67d37397d
lrollus <loic.rollus@cytomine.com>
2021-12-23 10:19:35 +0100

    log xml dir

--- scripts/ciTest.sh
+++ scripts/ciTest.sh
@@ -11,9 +11,13 @@ VERSION_NUMBER=$(<"$file")
 echo "Launch tests for $VERSION_NUMBER"
 
 docker build --rm -f scripts/docker/Dockerfile-test.build --build-arg VERSION_NUMBER=$VERSION_NUMBER -t  cytomine/cytomine-core-spring-test .
-mkdir ./ci/surefire-reports
+mkdir ./ci/reports
 
 containerId=$(docker create --network scripts_default --link nginxTest:localhost-core --link postgresqltest:postgresqltest --link mongodbtest:mongodbtest --link rabbitmqtest:rabbitmqtest -v "$PWD"/ci/reports:/app/build/test-results cytomine/cytomine-core-spring-test )
 #docker network connect scripts_default $containerId
 docker start -ai  $containerId
+
+docker exec -it $containerId du /app/build/test-results
+
+
 docker rm $containerId
