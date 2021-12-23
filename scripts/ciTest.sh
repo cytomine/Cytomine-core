d7b5482600106ea34b77bcdeadba0fa40c0a6a1b
lrollus <loic.rollus@cytomine.com>
2021-12-23 10:58:54 +0100

    fight with dir permission

--- scripts/ciTest.sh
+++ scripts/ciTest.sh
@@ -12,7 +12,7 @@ echo "Launch tests for $VERSION_NUMBER"
 docker build --rm -f scripts/docker/Dockerfile-test.build --build-arg VERSION_NUMBER=$VERSION_NUMBER -t  cytomine/cytomine-core-spring-test .
 mkdir -p ./ci/reports/test
 
-containerId=$(docker create --network scripts_default --link nginxTest:localhost-core --link postgresqltest:postgresqltest --link mongodbtest:mongodbtest --link rabbitmqtest:rabbitmqtest -v "$PWD"/ci/reports:/app/build/test-results cytomine/cytomine-core-spring-test )
+containerId=$(docker create --network scripts_default --link nginxTest:localhost-core --link postgresqltest:postgresqltest --link mongodbtest:mongodbtest --link rabbitmqtest:rabbitmqtest --user $(id -u):$(id -g) -v "$PWD"/ci/reports:/app/build/test-results cytomine/cytomine-core-spring-test )
 #docker network connect scripts_default $containerId
 docker start -ai  $containerId
 
