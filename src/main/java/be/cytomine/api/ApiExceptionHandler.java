68ecef27ee2444b1692d64f9443e74a9d27e11dd
lrollus <loic.rollus@cytomine.com>
2021-12-28 09:19:42 +0100

    controller catching error

--- src/main/java/be/cytomine/api/ApiExceptionHandler.java
+++ src/main/java/be/cytomine/api/ApiExceptionHandler.java
@@ -1,15 +1,15 @@
 package be.cytomine.api;
 
 
+import be.cytomine.exceptions.CytomineMethodNotYetImplementedException;
 import be.cytomine.exceptions.ForbiddenException;
 import be.cytomine.exceptions.ObjectNotFoundException;
+import be.cytomine.exceptions.WrongArgumentException;
 import be.cytomine.utils.JsonObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Map;
@@ -35,5 +35,22 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 .body(jsonObject.toJsonString());
     }
 
+    @ExceptionHandler(CytomineMethodNotYetImplementedException.class)
+    public ResponseEntity<?> handleException(CytomineMethodNotYetImplementedException exception) {
+        exception.printStackTrace();
+        JsonObject jsonObject = JsonObject.of("errors", Map.of("message",  "Method is not yet implemented"));
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(jsonObject.toJsonString());
+    }
+
+
+    @ExceptionHandler(WrongArgumentException.class)
+    public ResponseEntity<?> handleException(WrongArgumentException exception) {
+        JsonObject jsonObject = JsonObject.of("errors", Map.of("message",  exception.getMessage()));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(jsonObject.toJsonString());
+    }
 
 }
