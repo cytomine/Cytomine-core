7be5b16e28e164fe73b1edbfab835f94e1784064
lrollus <loic.rollus@cytomine.com>
2022-04-20 15:11:19 +0200

    delete image (if orphans) when delete project

--- grails-app/services/be/cytomine/project/ProjectService.groovy
+++ grails-app/services/be/cytomine/project/ProjectService.groovy
@@ -37,6 +37,7 @@ import be.cytomine.utils.ModelService
 import be.cytomine.utils.Task
 import be.cytomine.utils.Utils
 import grails.converters.JSON
+import grails.plugin.springsecurity.SpringSecurityUtils
 import groovy.sql.GroovyResultSet
 import groovy.sql.Sql
 import org.codehaus.groovy.grails.web.json.JSONObject
@@ -71,6 +72,9 @@ class ProjectService extends ModelService {
     def notificationService
     def projectRepresentativeUserService
     def ontologyService
+    def configurationService
+    def abstractImageService
+
 
     def currentDomain() {
         Project
@@ -893,6 +897,12 @@ class ProjectService extends ModelService {
         ImageInstance.findAllByProject(project).each {
             imageInstanceService.delete(it,transaction,null, false)
         }
+    }
+
+    def deleteDependentAbstractImage(Project project, Transaction transaction,Task task=null) {
+        SpringSecurityUtils.doWithAuth("superadmin", {
+            abstractImageService.deleteUnusedAbstractImageAndTheirFiles(ImageInstance.findAllByProject(project))
+        })
     }
 
 //    def deleteDependentReviewedAnnotation(Project project, Transaction transaction,Task task=null) {
