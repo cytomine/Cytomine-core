bc7e778d71c778c5a9482c892f87a0a3755aba14
lrollus <loic.rollus@cytomine.com>
2022-06-29 13:34:04 +0200

    chore: private release

--- scripts/ciPublishJarPrivate.sh
+++ scripts/ciPublishJarPrivate.sh
@@ -10,7 +10,7 @@ VERSION_NUMBER=$(<"$file")
 
 echo "Build for $VERSION_NUMBER"
 ssh -p 50004 cytomine@185.35.173.82 "mkdir -p /data/core/$CUSTOMER/$VERSION_NUMBER"
-scp -P 50004 ./ci/Core.jar cytomine@185.35.173.82:/data/core/$CUSTOMER/$VERSION_NUMBER/Core.jar
+scp -P 50004 ./ci/cytomine.jar cytomine@185.35.173.82:/data/core/$CUSTOMER/$VERSION_NUMBER/Core.jar
 
 CORE_PATH="/data/core/$CUSTOMER/$VERSION_NUMBER/Core.jar"
 echo $CORE_PATH
