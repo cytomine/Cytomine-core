c27f1d0592b2fd614261aad81c8442b5afb74ad6
lrollus <loic.rollus@cytomine.com>
2022-09-08 13:04:23 +0200

    fix: typo

--- scripts/ciBuildVersion.sh
+++ scripts/ciBuildVersion.sh
@@ -23,7 +23,7 @@ elif [[ $gitLongTag =~ v[0-9]+.[0-9]+.[0-9]+-EE-beta.[0-9]+-0-[0-9a-g]{8,9}$ ]];
   versionNumber=$(echo $gitLongTag | sed -r "s/([0-9]+\.[0-9]+\.[0-9]+-EE-beta.[0-9]+)-[0-9]+-.+/\1/")
 elif [[ $gitLongTag =~ v[0-9]+.[0-9]+.[0-9]+-EE-0-[0-9a-g]{8,9}$ ]]; then
   echo "OFFICIAL EE"
-  versionNumber=$(echo $gitLongTag | sed -r "s/([0-9]+\.[0-9]+\.[0-9]+)-[0-9]+-EE.+/\1/")
+  versionNumber=$(echo $gitLongTag | sed -r "s/([0-9]+\.[0-9]+\.[0-9]+)-[0-9]+-EE-.+/\1/")
 else
   echo "WARNING: invalid tag for an official release $gitLongTag"
   versionNumber=$branchName-$(date "+%Y%m%d%H%M%S")-SNAPSHOT
