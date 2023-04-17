7d8a2d8f0bbfcbc94b1d0cc7af13ab0063dd3cba
Ba Thien Le <bathien.le.pro@gmail.com>
2023-04-17 17:15:18 +0200

    feat: add metadata search using elastic

---
+++ src/main/java/be/cytomine/api/controller/meta/RestMetadataController.java
@@ -0,0 +1,43 @@
+package be.cytomine.api.controller.meta;
+
+/*
+ * Copyright (c) 2009-2023. Authors: see NOTICE file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import be.cytomine.api.controller.RestCytomineController;
+import be.cytomine.service.search.MetadataSearchService;
+import be.cytomine.utils.JsonObject;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/")
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class RestMetadataController extends RestCytomineController {
+
+    private final MetadataSearchService metadataSearchService;
+
+    @PostMapping("search.json")
+    public ResponseEntity<String> search(@RequestBody JsonObject body) {
+        log.debug("Rest request to search image using metadata:" + body);
+        return responseSuccess(metadataSearchService.search(body));
+    }
+}
---
+++ src/main/java/be/cytomine/service/search/MetadataSearchService.java
@@ -0,0 +1,82 @@
+package be.cytomine.service.search;
+
+/*
+ * Copyright (c) 2009-2023. Authors: see NOTICE file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import be.cytomine.domain.meta.Property;
+import be.cytomine.utils.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.StringQuery;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+@Transactional
+public class MetadataSearchService {
+
+    private ElasticsearchOperations operations;
+
+    public MetadataSearchService(ElasticsearchOperations operations) {
+        this.operations = operations;
+    }
+
+    private StringQuery buildStringQuery(JsonObject parameters) {
+        String prefixQuery = "{\"dis_max\": { \"queries\": [";
+        String suffixQuery = "]}}";
+        String termsString = String.format("{ \"terms\": { \"domain_ident\": %s } },", parameters.get("imageIds"));
+        StringBuilder queries = new StringBuilder();
+
+        HashMap<String, Object> filters = (HashMap) parameters.get("filters");
+        for (Map.Entry<String, Object> entry : filters.entrySet()) {
+            String queryString = String.format(
+                "{ \"query_string\": { \"query\": \"*%s*\", \"default_field\": \"value\" } },",
+                entry.getValue()
+            );
+            String matchString = String.format("{\"match\": {\"key\": \"%s\"}}", entry.getKey());
+            queries.append(String.format("{\"bool\": { \"must\": [ %s ] } }", queryString + termsString + matchString));
+        }
+
+        log.debug("Elasticsearch query: " + prefixQuery + queries + suffixQuery);
+
+        return new StringQuery(prefixQuery + queries + suffixQuery);
+    }
+
+    public List<Long> search(JsonObject body) {
+        SearchHits<Property> searchHits = operations.search(
+            this.buildStringQuery(body),
+            Property.class,
+            IndexCoordinates.of("properties")
+        );
+        log.debug(String.format("Total hits: %d", searchHits.getTotalHits()));
+
+        List<Long> ids = new ArrayList<>();
+        for (SearchHit<Property> hit : searchHits) {
+            ids.add(hit.getContent().getDomainIdent());
+        }
+
+        return ids;
+    }
+}
