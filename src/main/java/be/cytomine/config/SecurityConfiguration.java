d2b1edbe6abfaa05296a8ea6198df6251b73f371
Mohamed Eltahir <mohamed.eltahir@cytomine.com>
2023-11-22 12:34:35 +0100

    feat: :sparkles: [DONE] Configuration Migration
    Configuration Migration: - In 6.0, `@Configuration` is removed from `@EnableWebSecurity`, so we added explicitly.  - `.antMatchers()` deprecated start using `.requestMatchers()` however not sure how to use `.securityMatcher()` which one to replace with which ? just spammed `.requestMatchers()` everywhere. :upside_down:   - Biggest one is this: Stopped Using `WebSecurityConfigurerAdapter`.     - **TODO more testing with the last step:** still not sure about my method to expose `AuthenticationManager` Bean not trying to migrate this:     ```     @Override     protected void configure(AuthenticationManagerBuilder auth) throws Exception {         auth.userDetailsService(domainUserDetailsService).passwordEncoder(passwordEncoder());     }     ```      my solution is this function `public AuthenticationManager authManager(UserDetailsService detailsService)`.

--- src/main/java/be/cytomine/config/SecurityConfiguration.java
+++ src/main/java/be/cytomine/config/SecurityConfiguration.java
@@ -23,26 +23,30 @@ import be.cytomine.config.security.JWTConfigurer;
 import be.cytomine.security.jwt.TokenProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.switchuser.SwitchUserFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
     private final TokenProvider tokenProvider;
 
@@ -112,24 +116,38 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //        encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
 //        encoders.put("sha256", new MessageDigestPasswordEncoder("SHA-256"));
 //
-//
 //        return new DelegatingPasswordEncoder(idForEncode, encoders);
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(domainUserDetailsService).passwordEncoder(passwordEncoder());
+      // TODO: we are trying to migrate this to exposing a Bean is it really working? NOT sure more testing is needed
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(domainUserDetailsService).passwordEncoder(passwordEncoder());
+//    }
+
+    /**
+     * configures Spring Security to use your DomainUserDetailsService to fetch user details from a custom source (DB which's SecUserRepository)
+     * and to use the provided PasswordEncoder to encode and verify passwords.
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public AuthenticationManager authManager(UserDetailsService detailsService){
+        DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
+        daoProvider.setUserDetailsService(detailsService);
+        daoProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(daoProvider);
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring()
-                .antMatchers(HttpMethod.OPTIONS, "/**")
-                .antMatchers("/app/**/*.{js,html}")
-                .antMatchers("/i18n/**")
-                .antMatchers("/content/**")
-                .antMatchers("/h2-console/**")
-                .antMatchers("/test/**");
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(HttpMethod.OPTIONS, "/**")
+                .requestMatchers("/app/**/*.{js,html}")
+                .requestMatchers("/i18n/**")
+                .requestMatchers("/content/**")
+                .requestMatchers("/h2-console/**")
+                .requestMatchers("/test/**");
     }
 
     private JWTConfigurer securityConfigurerAdapter() {
@@ -138,11 +156,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     /**
      * Spring Security 6 Require Explicit Saving of SecurityContextRepository
+     *
      * @param http the {@link HttpSecurity} to modify
      * @throws Exception
      */
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 // @formatter:off
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .csrf()
@@ -157,17 +176,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //            .permitAll()
         .and()
             .authorizeRequests()
-            .antMatchers("/api/authenticate").permitAll()
-            .antMatchers("/api/register").permitAll()
-            .antMatchers("/api/activate").permitAll()
-            .antMatchers("/api/account/resetPassword/init").permitAll()
-            .antMatchers("/api/account/resetPassword/finish").permitAll()
-            .antMatchers("/api/login/impersonate*").hasAuthority("ROLE_ADMIN")
-            .antMatchers("/api/**").authenticated()
-            .antMatchers("/session/admin/**").authenticated()
-            .antMatchers(HttpMethod.GET, "/server/**").permitAll()
-            .antMatchers(HttpMethod.POST, "/server/**").permitAll()
-            .antMatchers("/**").permitAll()
+            .requestMatchers("/api/authenticate").permitAll()
+            .requestMatchers("/api/register").permitAll()
+            .requestMatchers("/api/activate").permitAll()
+            .requestMatchers("/api/account/resetPassword/init").permitAll()
+            .requestMatchers("/api/account/resetPassword/finish").permitAll()
+            .requestMatchers("/api/login/impersonate*").hasAuthority("ROLE_ADMIN")
+            .requestMatchers("/api/**").authenticated()
+            .requestMatchers("/session/admin/**").authenticated()
+            .requestMatchers(HttpMethod.GET, "/server/**").permitAll()
+            .requestMatchers(HttpMethod.POST, "/server/**").permitAll()
+            .requestMatchers("/**").permitAll()
 
 //        .and()
 //            .httpBasic()
@@ -185,6 +204,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionManagement((sessions) -> sessions
                     .requireExplicitAuthenticationStrategy(true)
             );
+
+        return http.build();
         // @formatter:on
     }
 
