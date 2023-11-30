f235adc80f325ddf05a6ad3ce19400478d4f4ced
Ulysse Rubens <ulysse.rubens@cytomine.com>
2023-11-30 17:27:40 +0100

    tests: fix attributes in LdapIdentityAuthenticationProviderTests

--- src/test/java/be/cytomine/security/authentication/LdapIdentityAuthenticationProviderTests.java
+++ src/test/java/be/cytomine/security/authentication/LdapIdentityAuthenticationProviderTests.java
@@ -65,10 +65,10 @@ public class LdapIdentityAuthenticationProviderTests {
         builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_SEARCH, "OU=users,DC=mtr,DC=com");
         builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_ATTRIBUTES, "cn,sn,givenname,mail");
         builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_PASSWORD_ATTRIBUTE_NAME, "password");
-        builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_USERNAME_ATTRIBUTE_NAME, "username");
+        builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_USERNAME_ATTRIBUTE_NAME, "cn");
         builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_EMAIL_ATTRIBUTE_NAME, "mail");
-        builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_FIRSTNAME_ATTRIBUTE_NAME, "firstname");
-        builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_LASTNAME_ATTRIBUTE_NAME, "lastname");
+        builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_FIRSTNAME_ATTRIBUTE_NAME, "givenname");
+        builder.given_a_configuration(ConfigurationService.CONFIG_KEY_LDAP_LASTNAME_ATTRIBUTE_NAME, "sn");
     }
 
     @Test
