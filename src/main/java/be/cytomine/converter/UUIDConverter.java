8cbe512ec7ce54fe3791367064931a27116adef1
siddig.hamed <siddig.hamed@cytomine.com>
2024-03-07 16:20:30 +0100

    fix: custom UUID converter annotation

--- src/main/java/be/cytomine/converter/UUIDConverter.java
+++ src/main/java/be/cytomine/converter/UUIDConverter.java
@@ -2,9 +2,10 @@ package be.cytomine.converter;
 
 
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 
 import java.util.UUID;
-
+@Converter
 public class UUIDConverter implements AttributeConverter<UUID, String> {
     @Override
     public String convertToDatabaseColumn(UUID attribute) {
