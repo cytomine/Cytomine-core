e71802b147154c4306ffbc0c6392db50356050f1
siddig.hamed <siddig.hamed@cytomine.com>
2024-05-27 17:54:00 +0200

    fix: Oauth2 config
    - pass OIDC traffic through proxy
    - do config without issuer-uri validation

--- src/main/java/be/cytomine/config/SecurityConfiguration.java
+++ src/main/java/be/cytomine/config/SecurityConfiguration.java
@@ -29,15 +29,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -126,6 +122,8 @@ public class SecurityConfiguration {
                 );
         http.oauth2ResourceServer((oauth2) -> oauth2
                 .jwt(jwtAuthConverter -> jwtAuthConverter.jwtAuthenticationConverter(customJwtAuthConverter)));
+
         return http.build();
     }
 }
+
