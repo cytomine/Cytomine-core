6df0ac95509cb3c5461d69e94ace91affbc599c8
Ba Thien LE <bathien.le.pro@gmail.com>
2025-04-11 15:44:37 +0200

    feat: preserve task runs history

--- src/main/java/be/cytomine/repository/appengine/TaskRunRepository.java
+++ src/main/java/be/cytomine/repository/appengine/TaskRunRepository.java
@@ -1,5 +1,6 @@
 package be.cytomine.repository.appengine;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +11,6 @@ import be.cytomine.domain.appengine.TaskRun;
 
 public interface TaskRunRepository extends JpaRepository<TaskRun, Long>, JpaSpecificationExecutor<TaskRun> {
     Optional<TaskRun> findByProjectIdAndTaskRunId(Long projectId, UUID taskRunId);
+
+    List<TaskRun> findAllByProjectId(Long projectId);
 }
